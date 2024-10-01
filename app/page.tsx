250867adcfd3b5186c87d37f0bc7206f0a9deaad
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-10-01 12:27:31 -0300

    Primera tienda  con Next.js y SQLite

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 import db from '../db';
+import Image from 'next/image';
+import { Product } from './types';  // Asegúrate de importar la interfaz Product
 
 export default async function Home() {
   // Obtener los productos desde la base de datos SQLite
-  const products = db.prepare('SELECT * FROM products').all();
+  const products: Product[] = db.prepare('SELECT * FROM products').all();
 
   return (
     <div>
       <h1>Productos Disponibles</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.map((product) => (
+        {products.map((product: Product) => (
           <div key={product.id} className="border p-4">
-            <img src={product.image} alt={product.name} />
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={300}
+              height={200}
+            />
             <h2>{product.name}</h2>
             <p>{product.description}</p>
             <p>{product.price} ARS</p>
---
+++ app/types.ts
@@ -0,0 +1,8 @@
+// app/types.ts
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
